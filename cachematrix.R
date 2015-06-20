eda43658f29e667dc9a094bafd8e4f94a42343c9
bim <bim@hm.dev0>
2015-06-20 21:41:29 +0300

    Version #1 of makeCacheMatrix() and cacheSolve ()
     Committer: Andrey Zhdanov <bim@hm.dev0>
    
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   cachematrix.R
    
     Untracked files:
    	.cachematrix.R.swp

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix(x = matrix() )function create a list object
+## that contains special function
+##1. set - to set the value of matrix
+##2. get - to get the value of matrix
+##3. setinverse - to set the value of inverse matrix
+##4. getinverse - to get the value of inverse matix
+##
+## cacheSolve(x, ...) function return inverse matrix of x, where x is  
+## a special object creted by makeCacheMatrix
+## in case when inverse matrix is already calculated function returns 
+## cached value of inverse matrix otherwise it calculates inverse
+## with help of solve() function 
 
-## Write a short comment describing this function
-
+## makeCacheMatrix(x = matrix()) create special list to set and get
+## matrix and its inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverse <- NULL
+	set <- function (y) {
+		x <<- y
+		inverse <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(solution) inverse <<- solution
+	getinverse <- function() inverse
+	list (set = set, get = get, setinverse = setinverse,
+		getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve (x, ...) Return inverse matrix, function calculates
+## inverse by solve() call or returns cached in x value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	inverse <- x$getinverse()
+	if(!is.null(inverse)) {
+		message("getting cached value")
+		return (inverse)
+	}
+	data <- x$get()
+	inverse <- solve(data, ...)
+	x$setinverse (inverse)
+	inverse
 }
