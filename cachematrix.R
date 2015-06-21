5a996c94b1940ee67de1f81f26615f426f4217b3
bim <bim@hm.dev0>
2015-06-21 11:12:04 +0300

    Committer: bim <bim@hm.dev0>
     On branch master
     Changes to be committed:
    	modified:   cachematrix.R
    
    Fix some mistake in comments in file cachematrix.R
    The code has not been changed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,15 @@
-## makeCacheMatrix(x = matrix() )function create a list object
-## that contains special function
-##1. set - to set the value of matrix
-##2. get - to get the value of matrix
-##3. setinverse - to set the value of inverse matrix
-##4. getinverse - to get the value of inverse matix
+## makeCacheMatrix(x = matrix()) function create a list object
+## that contains special functions
+##1. set() - to set the value of matrix
+##2. get() - to get the value of matrix
+##3. setinverse() - to set the value of inverse matrix
+##4. getinverse() - to get the value of inverse matix
 ##
-## cacheSolve(x, ...) function return inverse matrix of x, where x is  
-## a special object creted by makeCacheMatrix
+## cacheSolve(x, ...) function return inverse matrix of x, where x is 
+## a special object creted by makeCacheMatrix()
 ## in case when inverse matrix is already calculated function returns 
 ## cached value of inverse matrix otherwise it calculates inverse
-## with help of solve() function 
+## matrix with help of solve() function 
 
 ## makeCacheMatrix(x = matrix()) create special list to set and get
 ## matrix and its inverse matrix
@@ -42,3 +42,6 @@ cacheSolve <- function(x, ...) {
 	x$setinverse (inverse)
 	inverse
 }
+
+# vim: ts=8
+
